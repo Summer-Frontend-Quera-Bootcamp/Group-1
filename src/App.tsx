54c7baddcfc3d2bbe5fa9f57bee0e3c9b0406b66
Amirhoseinmiri <amirhoseinmiri444@gmail.com>
2023-09-20 22:28:36 +0330

    add pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import "./App.scss";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Test from "../pages/Test";
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Login</h1>} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/register" element={<h1>register</h1>} />
+          <Route path="/forgot" element={<h1>forgot</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
