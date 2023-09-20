92ebf62bf80e6baf376f65bf65382314cc8990fc
Farnam <farnam.faryar@gmail.com>
2023-09-20 09:07:25 +0430

    add customized colors to tailwind-config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      // Customized Colors
+      cyan: "#208D8E",
+      gray: "#7D828C",
+      "light-blue": "#A5E4F8",
+      danger: "#9F0000",
+      black: "#000000",
+      "light-black": "#1E1E1E",
+      pink: "#F92E8F",
+      orange: "#F98F2E",
+      green: "#43BB0B",
+      red: "#FC0733",
+    },
     extend: {},
   },
   plugins: [],
