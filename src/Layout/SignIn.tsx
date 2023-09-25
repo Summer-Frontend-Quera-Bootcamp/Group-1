6cdab7f1b6802c17553d1e9d8debbd943206ec3b
alireza_he <alirezaheibati1@gamil.com>
2023-09-25 15:22:14 +0330

    fix scroll & fix component position

--- src/Layout/SignIn.tsx
+++ src/Layout/SignIn.tsx
@@ -1,27 +1,51 @@
-// import { Link } from "react-router-dom"
-//remove comment when fix
-const SignInLayout = () => {
-    return (
-      <div>
-        <div className="h-[55px] w-[1280px]  mt-[80px] mr-[80px] ml-[80px]  flex items-center justify-between ">
-          <div className="w-[235px] h-[55px] text-[32px] font-extrabold capitalize not-italic leading-normal text-right linear-gradient-heading ">
-            کوئرا تسک منیجر
-          </div>
-          <div className="flex justify-center items-center">
-            <div className=" text-[16px] not-italic text-right font-medium capitalize ml-[7px]">
-              قبلا ثبت‌نام کرده‌ای؟
-              {/* <Link to="#">
-                          قبلا ثبت‌نام کرده‌ای؟
-                          </Link> */}
-            </div>
-            <button className="w-[75px] h-[40px] p-[10px] border-solid border-10 rounded-md bg-brand-primary text-sm text-white font-extrabold capitalize not-italic leading-normal">
-              ورود
-            </button>
-          </div>
-        </div>
-        <div className=" w-[100vw] h-[60vh] linear-gradient mt-[315px]  skew-y-[-5deg] "></div>
+import { Link } from "react-router-dom";
+
+type SignInLayoutProps = {
+  showSignup: boolean;
+  children?: JSX.Element | JSX.Element[];
+};
+
+const SignInLayout: React.FC<SignInLayoutProps> = ({
+  showSignup,
+  children,
+}): JSX.Element => {
+  return (
+    <div>
+      {/* Place for more Components as Children */}
+      <div className="flex justify-center items-center w-60 h-80  bg-brand-secondary absolute top-[25%] right-[40%]">
+        {children}
       </div>
-    );
-  };
-  export default SignInLayout;
-  
+      <div className="flex justify-between mt-[80px] ml-[80px] mr-[80px]">
+        <h1 className="text-[32px] linear-gradient-heading font-extrabold items-baseline">
+          کوئرا تسک منیجر
+        </h1>
+        <div className="flex items-baseline gap-2">
+          {showSignup ? (
+            <>
+              <p className="text-base font-medium">ثبت‌نام نکرده‌ای؟</p>
+              <Link
+                to={"/signup"}
+                className="flex font-extrabold text-sm w-[75px] h-[40px] p-[10px] rounded-md text-white bg-brand-primary justify-center items-center ]"
+              >
+                ثبت‌نام
+              </Link>{" "}
+            </>
+          ) : (
+            <>
+              <p className="text-base font-medium">قبلا ثبت‌نام کرده‌ای؟</p>
+              <Link
+                to={"/signup"}
+                className="flex font-extrabold text-sm w-[75px] h-[40px] p-[10px] rounded-md text-white bg-brand-primary justify-center items-center ]"
+              >
+                ورود
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+
+      <div className=" w-[100%] h-[557px] linear-gradient mt-[430px] skew-y-[-7deg] fixed top-0 right-0  z-[-100]"></div>
+    </div>
+  );
+};
+export default SignInLayout;
