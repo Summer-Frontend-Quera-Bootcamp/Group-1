4cdae6a9e7bc5a7c87e87ce43cf6f00228ab80f6
Farnam <farnam.faryar@gmail.com>
2023-09-26 13:21:45 +0330

    add pages to app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Test from "./pages/Test";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Forgot from "./pages/Forgot";
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Login</h1>} />
+          <Route path="/" element={<SignIn />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/forgot" element={<Forgot />} />
           <Route path="/test" element={<Test />} />
         </Routes>
       </BrowserRouter>
