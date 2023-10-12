86a89159791e17173783c5cb5c31d92d84f69645
Mohammadreza <hozoorimohammadreza1@gmail.com>
2023-10-12 13:22:07 +0330

    added closeing Filter component function

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -3,16 +3,19 @@ import close__icone from "./../../assets/Icons/close_icon.svg"
 import {Filters} from "./Scripts"
 
 const Filter:React.FC = (): JSX.Element => {
-    // Filter settings
+    // Filters settings
     const [showFilters, setShowFilters] = useState(true);
 
-    return (
+    // Filter Setting
+    const [showFilter, setShowFilter] = useState(true);
+
+    return showFilter ? (
         <div className={`flex flex-col items-stretch justify-start gap-[19px] box-border w-[717.7px] h-[206px] pt-[15px] pr-[20.7px] pb-auto pl-[24px] bg-[#FFF] rounded-[8px] shadow-[0px_8px_12px_0px_rgba(0,0,0,0.20)]`}>
             <div className={`flex flex-row items-center justify-between`}>
                 <div className={`text-[24px] font-extrabold`}>
                     فیلترها
                 </div>
-                <div className={`cursor-pointer select-none`}>
+                <div onClick={() => setShowFilter(false)} className={`cursor-pointer select-none`}>
                     <img src={close__icone} alt=""/>
                 </div>
             </div>
@@ -23,7 +26,7 @@ const Filter:React.FC = (): JSX.Element => {
                 <div onClick={() => setShowFilters(true)} className={`text-[12px] font-extrabold text-[#208D8E] cursor-pointer select-none`}>افزودن فیلتر جدید</div>
             </div>
         </div>
-    );
+    ) : <></>
 }
 
 export default Filter;
