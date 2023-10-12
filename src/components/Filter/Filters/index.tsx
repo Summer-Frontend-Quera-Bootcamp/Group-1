b96079fbea91dc86f2a322984796b479d6071f0a
Mohammadreza <hozoorimohammadreza1@gmail.com>
2023-10-12 12:57:58 +0330

    a new component named filters created and some changes applied in Filter component and Scripts

---
+++ src/components/Filter/Filters/index.tsx
@@ -0,0 +1,53 @@
+import close__icone from "../../../assets/Icons/close_icon.svg";
+import {IsOrNot, Tag, Where} from "../Scripts";
+import {JSX, useState} from "react";
+
+interface IFilters {
+    showMethodFilters: (newValue: boolean) => void;
+}
+const Filters: React.FC<IFilters> = ({showMethodFilters}):JSX.Element => {
+    // IsOrNot settings
+    const [showION, setShowION] = useState(false);
+
+    // Tag settings
+    const [showTag, setShowTag] = useState(false)
+
+    // Where settings
+    const [showWhere, setShowWhere] = useState(false)
+
+
+    return (
+        <div className={`flex flex-row items-center justify-between animate__animated animate__fadeIn`}>
+            <div className={`relative flex flex-row items-center justify-start gap-[8px] text-[14px]`}>
+                <div>تسک‌هایی که</div>
+                <div onClick={() => setShowWhere(!showWhere)} className={`flex flex-row items-center justify-between relative w-[182px] border border-solid border-[#E9EBF0] rounded-[6px] text-[12px] text-[#959595] box-border px-[8px] pt-[5px] pb-[4px] cursor-pointer select-none`}>
+                    <div>انتخاب کنید</div>
+                    <div><img src={close__icone} alt="" width={`18`}/></div>
+                </div>
+                {
+                    showWhere ? <Where showValue={showWhere} showMethod={setShowWhere} /> : null
+                }
+                <div>آن‌ها</div>
+                <div onClick={() => setShowTag(!showTag)} className={`flex flex-row items-center justify-between relative w-[146px] border border-solid border-[#E9EBF0] rounded-[6px] text-[12px] text-[#959595] box-border px-[8px] pt-[5px] pb-[4px] cursor-pointer select-none`}>
+                    <div>انتخاب کنید</div>
+                    <div><img src={close__icone} alt="" width={`18`}/></div>
+                </div>
+                {
+                    showTag ? <Tag showValue={showTag} showMethod={setShowTag} /> : null
+                }
+                <div onClick={() => setShowION(!showION)} className={`flex flex-row items-center justify-between relative w-[107px] border border-solid border-[#E9EBF0] rounded-[6px] text-[12px] box-border px-[8px] pt-[5px] pb-[4px] cursor-pointer select-none`}>
+                    <div>است</div>
+                    <div><img src={close__icone} alt="" width={`18`}/></div>
+                    {
+                        showION ? <IsOrNot /> : null
+                    }
+                </div>
+            </div>
+            <div onClick={() => showMethodFilters(false)} className={`self-end cursor-pointer select-none`}>
+                <img src={close__icone} alt=""/>
+            </div>
+        </div>
+    );
+}
+
+export default Filters;
--- src/components/Filter/Scripts/index.ts
+++ src/components/Filter/Scripts/index.ts
@@ -1,5 +1,6 @@
 import IsOrNot from "../IsOrNot";
 import Where from "../Where";
 import Tag from "../Tag";
+import Filters from "../Filters";
 
-export {IsOrNot, Tag, Where}
+export {IsOrNot, Tag, Where, Filters}
