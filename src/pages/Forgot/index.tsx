2b40ad10a5be6f17d940e21df5bc69f057f73034
ifarnami <farnam.faryar@gmail.com>
2023-10-22 17:15:40 +0330

    connect Forgot components to api and redirect to LinkSent page

--- src/pages/Forgot/index.tsx
+++ src/pages/Forgot/index.tsx
@@ -1,28 +1,46 @@
+import { useState } from "react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import AuthenticateLayout from "../../Layout/AuthenticateLayout";
 import AccountCard from "../../components/FormContainer/components/AccountCard";
 import Input from "../../components/FormContainer/components/Input";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { baseUrl } from "../../constants/api";
 
 type Values = {
   email: string;
 };
 
 const Forgot = () => {
+  const [apiError, setApiError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
   const initialValues = {
     email: "",
   };
 
-  const onSubmit = (values: Values) => {
-    const db = localStorage;
-    if (db.getItem("email") !== values.email) {
-      console.log("OOPS, You don't have any account here!");
-      alert("OOPS, You don't have any account here!");
-    } else {
-      console.log("Reset password link has been sent.");
-      alert("Reset password link has been sent.");
-    }
+  const onSubmit = async (values: Values) => {
+    setLoading(true);
+    await axios({
+      method: "post",
+      url: `${baseUrl}accounts/reset-password/`,
+      data: values,
+    })
+      .then(() => {
+        setLoading(false);
+        setApiError("");
+        navigate("/sent-link");
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (error.response.data.email) {
+          setApiError("پست الکترونیکی صحبح وارد کنید.");
+        } else if (error.response.data.detail) {
+          setApiError("کاربری با این ایمیل وجود ندارد.");
+        }
+      });
   };
 
   const validationSchema = Yup.object({
@@ -35,6 +53,15 @@ const Forgot = () => {
     <AuthenticateLayout showSignup={false}>
       {/* ------------------------------Formik------------------------------ */}
       <AccountCard title="فراموشی رمز عبور">
+        {/* Authentication Error Handling */}
+        {apiError ? (
+          <div className="text-center bg-red-700 text-white px-5 mb-5 rounded-md animate__animated animate__fadeIn">
+            {apiError}
+          </div>
+        ) : (
+          <></>
+        )}
+
         <Formik
           initialValues={initialValues}
           onSubmit={onSubmit}
@@ -45,14 +72,26 @@ const Forgot = () => {
             action="post"
             className="flex justify-between flex-col gap-2 mb-6"
           >
-            <Input label="ایمیل" type="email" autoCompleteField="email" />
+            <Input
+              label="ایمیل خود را وارد کنید."
+              type="email"
+              autoCompleteField="email"
+            />
 
-            <button
-              className="w-full flex justify-center items-center rounded-md h-10 bg-brand-primary text-white font-extrabold mb-4"
-              type="submit"
-            >
-              دریافت ایمیل بازیابی رمز عبور
-            </button>
+            {loading ? (
+              <div className="bg-brand-primary flex flex-row gap-2 justify-center items-center rounded-md h-10">
+                <div className="w-4 h-4 rounded-full bg-white animate-bounce"></div>
+                <div className="w-4 h-4 rounded-full bg-white animate-bounce [animation-delay:-.3s]"></div>
+                <div className="w-4 h-4 rounded-full bg-white animate-bounce [animation-delay:-.5s]"></div>
+              </div>
+            ) : (
+              <button
+                className="w-full flex justify-center items-center rounded-md h-10 bg-brand-primary text-white font-extrabold"
+                type="submit"
+              >
+                ثبت‌نام
+              </button>
+            )}
             <Link
               className="text-center font-extrabold text-brand-primary -mb-6"
               to={"/"}
