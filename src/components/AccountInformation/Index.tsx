a149272f6f5f780c975dedb0447638fe56cabc39
Eynvaav <142487085+Eynvaav@users.noreply.github.com>
2023-10-27 07:55:11 +0330

    some route and style fixes

--- src/components/AccountInformation/Index.tsx
+++ src/components/AccountInformation/Index.tsx
@@ -1,49 +1,100 @@
-import Input from "../FormContainer/components/Input.tsx";
-import { Formik, Form, ErrorMessage, Field, useFormik } from "formik";
-import * as Yup from "yup"
+import Input from '../FormContainer/components/Input.tsx';
+import { Formik, Form, ErrorMessage, Field, useFormik } from 'formik';
+import * as Yup from 'yup';
 import Profile from '../Setting';
 const AccountInformation = () => {
-const initialValues = {email: "",username: "", password:"",password1:"", password2:""}
+	const initialValues = {
+		email: '',
+		username: '',
+		password: '',
+		password1: '',
+		password2: '',
+	};
 
-const validationSchema = Yup.object({
-  email:Yup.string().required("ایمیل خود را وارد نکردید"),
-  username: Yup.string().required("نام کاربری خود را وارد نکردید"),
-  password:Yup.string().required("رمز عبور خود را وارد نکردید"),
-  password1: Yup.string().required("رمز عبور خود را تکرار نکرده اید")
-      .oneOf(
-        [Yup.ref("password")],
-        "رمز وارد شده با رمز انتخابی جدید یکسان نیست"
-      ),
-      password2:Yup.string().required("رمز عبور خود را تکرار نکرده اید")
-      .oneOf(
-        [Yup.ref("password1")],
-        "رمز وارد شده با رمز انتخابی جدید یکسان نیست"
-      )
-})
+	const validationSchema = Yup.object({
+		email: Yup.string().required('ایمیل خود را وارد نکردید'),
+		username: Yup.string().required('نام کاربری خود را وارد نکردید'),
+		password: Yup.string().required('رمز عبور خود را وارد نکردید'),
+		password1: Yup.string()
+			.required('رمز عبور خود را تکرار نکرده اید')
+			.oneOf(
+				[Yup.ref('password')],
+				'رمز وارد شده با رمز انتخابی جدید یکسان نیست'
+			),
+		password2: Yup.string()
+			.required('رمز عبور خود را تکرار نکرده اید')
+			.oneOf(
+				[Yup.ref('password1')],
+				'رمز وارد شده با رمز انتخابی جدید یکسان نیست'
+			),
+	});
 
-  return (
-    <div>
-      <Profile/>
+	return (
+		<div>
+			<Profile />
 
-    <Formik initialValues={initialValues} onSubmit={()=>{}} validationSchema={validationSchema} validateOnMount ><Form action="patch" className='flex flex-col items-center mb-2.5'>
-      
-          <h1 className='text-right mb-2'>اطلاعات حساب</h1>
-      <div className='flex flex-col mb-2.5'><Input type='email' label='ایمیل' autoCompleteField='email' /></div>
-      <div className='flex flex-col mb-2.5'><Input type='text' label='نام کاربری' autoCompleteField='text' /></div>
-      <div className='flex flex-col mb-2.5'><Input type='password' label='رمز عبور فعلی' autoCompleteField='password' /></div>
-      <div className='flex flex-col mb-2.5'><Input type='password1' label='رمز عبور جدید' autoCompleteField='password' /></div>
-      <div className='flex flex-col mb-2.5'><Input type='password2' label='تکرار رمز عبور جدید' autoCompleteField='password' /></div>
-      <Button/>
-    </Form>
-    </Formik>
-    </div>
-  )
+			<Formik
+				initialValues={initialValues}
+				onSubmit={() => {}}
+				validationSchema={validationSchema}
+				validateOnMount
+			>
+				<Form
+					action='patch'
+					className='flex flex-col items-center mb-2.5'
+				>
+					<h1 className='mt-20 text-right mb-2'>اطلاعات حساب</h1>
+					<div className='flex flex-col mb-2.5'>
+						<Input
+							type='email'
+							label='ایمیل'
+							autoCompleteField='email'
+						/>
+					</div>
+					<div className='flex flex-col mb-2.5'>
+						<Input
+							type='text'
+							label='نام کاربری'
+							autoCompleteField='text'
+						/>
+					</div>
+					<div className='flex flex-col mb-2.5'>
+						<Input
+							type='password'
+							label='رمز عبور فعلی'
+							autoCompleteField='password'
+						/>
+					</div>
+					<div className='flex flex-col mb-2.5'>
+						<Input
+							type='password1'
+							label='رمز عبور جدید'
+							autoCompleteField='password'
+						/>
+					</div>
+					<div className='flex flex-col mb-2.5'>
+						<Input
+							type='password2'
+							label='تکرار رمز عبور جدید'
+							autoCompleteField='password'
+						/>
+					</div>
+					<Button />
+				</Form>
+			</Formik>
+		</div>
+	);
+};
+
+function Button() {
+	return (
+		<button
+			type='submit'
+			className='flex  text-center items-center justify-start  rounded-md bg-brand-primary text-white w-64 px-12 py-2 font-extrabold'
+		>
+			ثبت تغییرات
+		</button>
+	);
 }
 
-function Button (){
-    return <button type='submit' className='flex  text-center items-center justify-start  rounded-md bg-teal-600 text-white w-64 px-12 py-2 font-extrabold'>ثبت تغییرات</button>
-}
-
-export default AccountInformation
-
-
+export default AccountInformation;
